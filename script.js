5811446ae7bcafc446bc751693f16d83fbc9bbae
damian.deleon <damianmdeleon@gmail.com>
2021-01-12 20:33:08 -0600

    updated with new files

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var timerCount;
+var timerElement = document.querySelector(".timer-count");
+var score = document.querySelector(".score");
+var startButton = document.querySelector("#start-button");
+
+
+//set up timer process
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function () {
+        timerCount = 120;
+        timerCount--;
+        timerElement.innerText = timerCount;
+        // if (timerCount >= 0) {
+            // Tests if win condition is met
+            
+                // Clears interval and stops timer
+                // clearInterval(timer);
+                // return
+            
+        // }
+        // Tests if time has run out
+        if (timerCount === 0) {
+            // Clears interval
+            clearInterval(timer);
+            // UserInfo();
+        }
+    }, 1000);
+}
+
+
+//function to accept the user's initials and score to save in the local storage
+
+//
+startButton.addEventListener("click", startTimer);
