c68521d588a88d891d0113927954f872da06e61b
damian.deleon <damianmdeleon@gmail.com>
2021-01-14 20:06:57 -0600

    set up questions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # code-quiz
+
+## Questions and Answers:  
+Credit:  Tutorialspoint website:  https://www.tutorialspoint.com/javascript/javascript_online_quiz.htm
+Question1:  Is JavaScript a case-sensitive language?
+Q1A1: Yes - correct
+Q1A2: No
+
+Question2: Which of the following functions returns a string value to uppoercase?
+Q2A1: toLocateUpperCase(),
+Q2A2: toUpperCase(), - correct
+Q2A3: toString(),
+Q2A4: substring()
+
+Question 3: Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?
+Q3A1: sup(),
+Q3A2: small(),
+Q3A3: strike(),
+Q3A4: sub() - correct
+
+Question 4: Which of the following function of Array object reverses the order of the elements of an array?
+Q3A1: reverse(), - correct
+Q3A2: push(),
+Q3A3: reduce(),
+Q3A4: reduceRight()
--- script.js
+++ script.js
@@ -32,7 +32,7 @@ container.setAttribute("style", "text-align:center");
 
 
 //set up timer process
-timerCount = 120;
+timerCount = 60;
 function startTimer() {
     // Sets timer
     var timer = setInterval(function () {
@@ -50,14 +50,60 @@ function startTimer() {
         if (timerCount === 0) {
             // Clears interval
             clearInterval(timer);
+            timerInfo.textContent = "Time's Up!";
             // UserInfo();
         }
     }, 1000);
 }
 
 //set up questions and answers
+var q1 = "Is JavaScript a case-sensitive language?"
+var q1Answers = {
+    //Q1A1 is correct
+    Q1A1: "Yes",
+    Q1A2: "No",
+    Q1A3: "Not always",
+    Q1A4: "Only when running HTML"
+}
+
+var q2 = "Which of the following functions returns a string value to uppoercase?"
+var q2Answers = {
+    //Q2A2 is correct
+    Q1A1: "toLocateUpperCase()",
+    Q2A2: "toUpperCase()",
+    Q2A3: "toString()",
+    Q2A4: "substring()"
+}
+
+var q3 = "Which of the following function of String object causes a string to be displayed as a subscript, as if it were in a <sub> tag?"
+var q3Answers = {
+    //Q3A4 is correct
+    Q3A1: sup(),
+    Q3A2: small(),
+    Q3A3: strike(),
+    Q3A4: sub()
+}
+
+var q4 = "Which of the following function of Array object reverses the order of the elements of an array?"
+var q4Answers = {
+    //Q3A1 is corect
+    Q3A1: reverse(),
+    Q3A2: push(),
+    Q3A3: reduce(),
+    Q3A4: reduceRight()
+}
+
 //question should show up as a text above the answers
+
 //answers should list out as a multiple choice by setting them up as objects and A1, A2, A3, A4 be the propery and the answer being the value of those properties.
+
+//if the user's input equals the the correct answer, move on to the next quesion and add 5 points
+
+//if the user's input does not equal the correct answer, substrct 20 seconds from the clock and move onto the next question
+    //take the timer count and create a new variable that takes the times's current count, minus 20.  Then use that new varible to and create a new startTimer function (so that it will reinitialize the countdown from it's current spot)
+
+
+
 
 //function to accept the user's initials and score to save in the local storage
 
